2c528ffd59677b8618bfbb9ac41c653a9e586179
urownsanctuary <112174648+urownsanctuary@users.noreply.github.com>
2026-04-20 21:24:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")  # токен из переменных Render
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
+
 sent_users = set()
 
-@dp.message()
+@dp.message_handler()
 async def stop_bot(message: types.Message):
     if message.from_user.id in sent_users:
         return
@@ -8,7 +17,12 @@ async def stop_bot(message: types.Message):
     sent_users.add(message.from_user.id)
 
     await message.answer(
-        "Сверки перенесены на сайт:\n"
+        "Коллеги, добрый вечер!\n\n"
+        "Сверки перенесены на сайт.\n\n"
+        "Пожалуйста, используйте новую систему:\n"
         "https://merch-web.onrender.com/login-page\n\n"
         "Бот больше не используется."
     )
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
